7999f65dfebb9a6003ab6007a544d5eb929b4a11
Satyam Katare <satyamkatare2006@gmail.com>
2026-04-04 20:10:03 +0530

    Solved two more questions

--- while_loop_practice/q6_prime_check.py
+++ while_loop_practice/q6_prime_check.py
@@ -1,2 +1,21 @@
 
-# Question : Check prime number. ()
+# Question : Check prime number. 
+
+n = int(input("tell me your number : "))
+
+i = 2
+is_prime = True
+
+while i <= n // 2:
+    if n % i == 0:
+        is_prime = False
+        break
+    
+    i += 1
+
+if is_prime and n > 1:
+    print("Prime number")
+
+else:
+    print("Not Prime")
+
